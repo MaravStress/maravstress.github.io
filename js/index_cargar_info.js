1f3b8ef3aaea5e96d59522486b79a049a187d691
MaravStress <eliamjesus@gmail.com>
2025-07-22 14:34:18 -0400

    Habilidades esta del kilo

--- js/index_cargar_info.js
+++ js/index_cargar_info.js
@@ -11,23 +11,23 @@ async function loadHabilidades() {
             // Renderizar cada grupo de habilidades
             grupos.forEach(grupo => {
                 const colDiv = document.createElement('div');
-                colDiv.className = 'col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center';
+                colDiv.className = 'col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center  ';
                 
                 const cardDiv = document.createElement('div');
-                cardDiv.className = 'card shadow h-100 w-100';
+                cardDiv.className = 'card shadow h-100 w-100 bg-blur rounded-4';
                 cardDiv.style.maxWidth = '350px';
                 
                 const cardHeader = document.createElement('div');
-                cardHeader.className = 'card-header text-center bg-white border-bottom-0';
+                cardHeader.className = 'card-header text-center bg-blur border-bottom-0 rounded-top-4';
                 cardHeader.innerHTML = `<strong class="fs-4">${grupo.nombre}</strong>`;
                 
                 const ul = document.createElement('ul');
-                ul.className = 'list-group list-group-flush';
+                ul.className = 'bg-transparent  list-group-flush ';
                 
                 // Agregar cada habilidad
                 grupo.habilidades.forEach(habilidad => {
                     const li = document.createElement('li');
-                    li.className = 'list-group-item';
+                    li.className = 'list-group-item py-1';
                     li.textContent = habilidad;
                     ul.appendChild(li);
                 });
@@ -65,7 +65,7 @@ async function loadProyectos() {
             // Renderizar cada proyecto
             proyectos.forEach(proyecto => {
                 const col = document.createElement('div');
-                col.className = 'col-md-4 mb-4';
+                col.className = 'col-md-6 mb-4';
                 
                 const cardDiv = document.createElement('div');
                 cardDiv.className = 'card h-100 shadow-sm';
@@ -118,3 +118,4 @@ function mostrarModalProyecto(proyecto) {
     const modal = new bootstrap.Modal(document.getElementById('proyectoModal'));
     modal.show();
 }
+
